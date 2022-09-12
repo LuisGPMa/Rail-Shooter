e5b81644331f6b653372972e955151669ce3f0b8
Marcelo Cohen <mflashbr@gmail.com>
2022-09-12 19:00:00 -0300

    Jogador simplificado e com diversos tipos de movimento

--- Main.gd
+++ Main.gd
@@ -1,14 +1,30 @@
 extends Node2D
 
 var total : float = 0
+const SPEED : int = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	update_score(total)
 	
-func _process(delta: float) -> void:
-	total += delta
-	update_score(total)
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_right"):
+		print("Right arrow")
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("ui_right"):
+		print("Move right")
+		position.x += SPEED * delta	
+
+#func _process(delta: float) -> void:
+#	total += delta
+#	update_score(total)
+#	pass
 	
 func update_score(current_score: float) -> void:
 	$Score.text = "Score: " + str(total)
+
+
+func _on_ScoreTimer_timeout() -> void:
+	total += 1
+	update_score(total)
