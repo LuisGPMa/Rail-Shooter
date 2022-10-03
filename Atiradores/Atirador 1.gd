04bd83cb69e387860e5931f8a877ec1111d1495d
LuisGPMa <luisgmarquetti@hotmail.com>
2022-10-03 19:01:18 -0300

    commit 2

---
+++ Atiradores/Atirador 1.gd
@@ -0,0 +1,69 @@
+extends KinematicBody2D
+
+export (int) var speed = 300
+export (float) var rotation_speed = 1.5
+
+export (PackedScene) var box : PackedScene
+
+var velocity := Vector2.ZERO
+var rotation_dir := 0
+
+# Onready inicializa como se estivesse no callback _ready
+onready var target = position
+onready var sprite := $Sprite
+#onready var audioPlayer := $AudioPlayer
+
+#onready var box := preload("res://Items/Box.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+func _input(event):
+	if event.is_action_pressed("click"):
+		target = get_global_mouse_position()
+
+func get_8way_input():
+	velocity = Vector2.ZERO
+	velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")	
+	if velocity.x > 0:
+		sprite.play("right")
+	elif velocity.x < 0:
+		sprite.play("left")
+	elif velocity.y > 0:
+		sprite.play("down")
+	elif velocity.y < 0:
+		sprite.play("up")
+	else:
+		sprite.stop()
+		sprite.frame = 0
+	velocity = velocity.normalized() * speed
+	#print(velocity)
+
+func get_rotation_input():
+	rotation_dir = 0
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("right"):
+		rotation_dir += 1
+	if Input.is_action_pressed("left"):
+		rotation_dir -= 1
+	if Input.is_action_pressed("down"):
+		velocity = Vector2(-speed, 0).rotated(rotation)
+	if Input.is_action_pressed("up"):
+		velocity = Vector2(speed, 0).rotated(rotation)
+
+func get_mouse_input():
+	look_at(get_global_mouse_position())
+	velocity = Vector2()
+	if Input.is_action_pressed("down"):
+		velocity = Vector2(-speed, 0).rotated(rotation)
+	if Input.is_action_pressed("up"):
+		velocity = Vector2(speed, 0).rotated(rotation)
+
+func get_point_and_click():
+	velocity = position.direction_to(target) * speed
+	if position.distance_to(target) <= 5:
+		velocity = Vector2.ZERO
+
--- Main.gd
+++ Main.gd
@@ -5,7 +5,7 @@ const SPEED : int = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	update_score(total)
+	pass
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_right"):
@@ -20,11 +20,3 @@ func _physics_process(delta: float) -> void:
 #	total += delta
 #	update_score(total)
 #	pass
-	
-func update_score(current_score: float) -> void:
-	$Score.text = "Score: " + str(total)
-
-
-func _on_ScoreTimer_timeout() -> void:
-	total += 1
-	update_score(total)
