9c43ee422262867837d781e27958fae43d25a351
LuisGPMa <luisgmarquetti@hotmail.com>
2022-10-10 18:57:55 -0300

    movemento sobre trilhos

--- Main.gd
+++ Main.gd
@@ -11,10 +11,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_right"):
 		print("Right arrow")
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_pressed("ui_right"):
-		print("Move right")
-		position.x += SPEED * delta	
+
 
 #func _process(delta: float) -> void:
 #	total += delta
